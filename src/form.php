b1eeefd60ceea4fd9441fe33efd23338c482e289
Yurem <84666021+Yurem12@users.noreply.github.com>
2022-05-27 12:51:35 +0200

    1.0

--- src/form.php
+++ src/form.php
@@ -1,20 +1,5 @@
 <?php 
 
-// sql auth vars
-
-$user = "root";
-$pass = "";
-
-try {
-    $db = new PDO('mysql:host=localhost;dbname=testdb', $user,$pass);
-
-
-
-} catch (PDOException $e) {
-    print "erreur :" . $e->getMessage() . "</br>";
-    die;
-}
-
 // affichage des valeurs
 
 foreach ($_POST as $key => $value) {
@@ -24,4 +9,42 @@ foreach ($_POST as $key => $value) {
     EOD; 
 }
 
+
+// sql auth vars
+
+$servername = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "testdb";
+
+
+// création de la connexion
+$conn = new mysqli($servername, $user, $pass, $dbname);
+// vérification de la conexion
+if ($conn->connect_error) {
+  die("Echec de la connexion: " . $conn->connect_error);
+}
+
+// fonction de vérification de présence de la donnée dans la base de données
+$getData = "SELECT * FROM clients";
+$result = $conn->query($getData);
+print_r($result);
+if ($result->num_rows > '0') {
+
+
+  echo "Cette ligne existe déjà dans la base de donnée.";
+
+  } else {
+    // si non inscription des données dans la base de données
+    $insertData = "INSERT INTO clients (Nom, Prenom, Mail, Msg)
+    VALUES ('$_POST[formSurname]', '$_POST[formName]', '$_POST[formMail]', '$_POST[formMsg]')";
+    
+  if ($conn->query($insertData) === true) {
+    echo "New record created successfully";
+  } else {
+    echo "Erreur: " . $insertData . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+  }
 ?>
